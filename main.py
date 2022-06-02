440f0a48e734e64d67596ca874f836a26afec57e
Evgenii Kichakov <kichakov.ea@gmail.com>
2022-06-02 20:04:01 +0300

    Add sort of 3 number from high to low

--- main.py
+++ main.py
@@ -1,20 +1,11 @@
+
+# Сортировка трёх чисел от большего к меньшему
 a, b, c = int(input()), int(input()), int(input())
-ma = (a, b, c)
-mi = (a, b, c)
-if ma == a and mi == b:
-    print(a)
-    print(b)
-    print(c)
-elif ma == b and mi == c:
-    print(b)
-    print(a)
-    print(c)
-elif ma == c and mi == a:
-    print(c)
-    print(b)
-    print(a)
-
-
+ma = max(a, b, c)
+mi = min(a, b, c)
+print(ma)
+print((a + b + c) - (ma + mi))
+print(mi)
 
 #a, b, c, d, f = int(input()), int(input()), int(input()), int(input()), int(input())
 #print('Наименьшее число = ', min(a,b,c,d,f))
