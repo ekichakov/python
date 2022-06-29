285bc10b5081a7753187a43d427bca33b7288567
ekichakov <kichakov.ea@gmail.com>
2022-06-29 16:37:23 +0300

    Add task Numerical triangle3 / Done

--- main.py
+++ main.py
@@ -6,13 +6,14 @@
 #11 12 13 14 15
 #16 17 18 19 20 21
 #...
-n = int(input())
-for i in range(1, n + 1):
-    for j in range(1, i + 1):
-        print(j, end='')
-    print()
-
-
+#n = int(input())
+#total = 1
+#for i in range(1, n + 1):
+#    for j in range(1, i + 1):
+#        print(total, end=' ')
+#        total += 1
+#    print()
+    
 
 
 # Код с очень долгим выполнением
